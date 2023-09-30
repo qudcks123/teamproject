cfdf954dd9db12b54050e09d005776696c4f75e7
gon06 <gon06@LAPTOP-DUH9UGLB>
2023-09-30 17:59:21 +0900

    과제 마무리

--- HelloPackage/src/prac/ObjectPropertyEx.java
+++ HelloPackage/src/prac/ObjectPropertyEx.java
@@ -6,8 +6,16 @@ class Point {
 		this.x = x;
 		this.y = y;
 	}
-}
 
+public String toString() {
+	return "Point(" + x + "," + y + ")";
+}
+public boolean equals(Object obj) {
+	Point p = (Point)obj;
+	if (x == p.x && y == p.y) return true;
+	else return false;
+}
+}
 public class ObjectPropertyEx {
 	public static void print(Object obj) {
 		System.out.println(obj.getClass().getName());
@@ -19,6 +27,23 @@ public class ObjectPropertyEx {
 		Point p = new Point(2, 3);
 		print(p);
 
+		System.out.println(p.toString());
+		System.out.println(p);
+		System.out.println(p + "입니다.");
+		
+		Point a = new Point(2,3);
+		Point b = new Point(2,3);
+		Point c = new Point(3,4);
+		
+		if(a == b) {
+			System.out.println("a == b");
+		}
+		if (a.equals(b)) {
+			System.out.println("a is equal to b");
+		}
+		if (a.equals(c)) {
+			System.out.println("a is equal to c");
+		}
 	}
 
 }
