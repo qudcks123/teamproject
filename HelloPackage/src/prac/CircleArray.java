e040ce1d91495191d047e73d8ade21dfe9d4bd4c
gon06 <gon06@10.50.47.188>
2023-10-06 22:02:36 +0900

    살려줘

---
+++ HelloPackage/src/prac/CircleArray.java
@@ -0,0 +1,24 @@
+package prac;
+class Circle{
+	int radius;
+	public Circle(int radius) {
+		this.radius = radius;
+	}
+	public double getArea() {
+		return 3.14*radius*radius;
+	}
+}
+public class CircleArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Circle []c;
+c = new Circle[5];
+
+for(int i = 0; i<c.length;i++)
+	c[i] = new Circle(i);
+for(int i = 0; i<c.length;i++)
+	System.out.print((int)(c[i].getArea()) + " ");
+	}
+
+}
