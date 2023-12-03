eb6cdb286f734e537e9752c2081df45349feceff
qudcks123 <qudcks3464@naver.com>
2023-12-03 18:42:11 +0900

    seat병찬

--- project/src/project/Seat.java
+++ project/src/project/Seat.java
@@ -1,14 +1,17 @@
 package project;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+
 import java.awt.*;
 
-
 public class Seat extends JFrame {
-	final static int NUM_BUTTONS =13;
+	final static int NUM_BUTTONS =12;
 	final static int NUM_SEATS = 100;
 	private Container cp;
 	private JRadioButton[] seat = new JRadioButton[101];
+	private JButton[] btn = new JButton[NUM_BUTTONS];
+
 	public Seat() {
 		setTitle("프방탄소년단 티켓");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,7 +20,6 @@ public class Seat extends JFrame {
 		createToolBarRight();
 		createToolBarUpper();
 		createSeatentire();
-		createSeatplacement();
 		setSize(1100,800);
 		setVisible(true);
 	}
@@ -68,72 +70,99 @@ public class Seat extends JFrame {
 		JPanel cp2a = new JPanel();
 		JPanel cp3 = new JPanel();
 		
-		cp2.setBackground(Color.BLUE);
-		cp3.setBackground(Color.GREEN);
 		cp1.setBackground(Color.WHITE);
+		cp2.setBackground(Color.WHITE);
+		cp2a.setBackground(Color.WHITE);
+		cp3.setBackground(Color.GREEN);
+		
 		
 		cp1.setLayout(new BorderLayout());
-		cp2.setLayout(new BorderLayout(5, 40));
-		GridLayout grid = new GridLayout(3,4);
+		cp2.setLayout(new BorderLayout(10, 40));
+		cp2a.setLayout(new BoxLayout(cp2a, BoxLayout.Y_AXIS));
+		GridLayout grid = new GridLayout(3,4,10,10);
 		cp3.setLayout(grid);
 		
 		cp2.setPreferredSize(new Dimension(cp2.getPreferredSize().width, 300));
 		
 		JLabel la1 = new JLabel("※원하시는 영역을 선택해주세요. 공연장에서 위치를 클릭하거나, 오른쪽의 좌석을 클릭해주세요.");
-
-		cp2.add(la1, BorderLayout.NORTH);
+		la1.setOpaque(true);
+		la1.setBackground(Color.LIGHT_GRAY);
+		Border border1 = BorderFactory.createLineBorder(Color.WHITE, 2); // 검은색 2픽셀 라인 테두리
+        la1.setBorder(border1);
+        
+        Font originalFont1 = la1.getFont();
+        int fontSize1 = originalFont1.getSize() + 5; // 원하는 만큼 크기를 늘릴 수 있습니다.
+        Font largerFont1 = originalFont1.deriveFont((float) fontSize1);
+        la1.setFont(largerFont1);
+        
 		
-		JLabel la2 = new JLabel(new ImageIcon("ImageFiles/구역이미지.PNG"));
-		JLabel la3 = new JLabel("구역 내 상단이 무대와 가까운 쪽입니다.");	
-		JLabel la4 = new JLabel("The upper end of the section is the closest area to the stage");
-		JLabel la5 = new JLabel("※가로로 (한줄로 나란히) 예매해 주세요.");
+		//JLabel la2 = new JLabel("ㅁㅁ",new ImageIcon("ImageFiles/구역이미지.PNG"), JLabel.CENTER);
+	//	la2.setHorizontalTextPosition(JLabel.CENTER);
+	//	la2.setVerticalTextPosition(JLabel.CENTER);
+		//la2.setAlignmentX(Component.CENTER_ALIGNMENT);
+		JLabel la3 = new JLabel("<html>구역 내 상단이 무대와 가까운 쪽입니다. <br> The upper end of the section is the closest area to the stage</html>");	
+		la3.setAlignmentX(Component.CENTER_ALIGNMENT); 		
+		
+		la3.setOpaque(true);
+		la3.setBackground(Color.white);
+		la3.setForeground(Color.BLUE);
+		Border border3 = BorderFactory.createLineBorder(Color.white, 2); // 검은색 2픽셀 라인 테두리
+        la3.setBorder(border3);
+        
+        Font originalFont3 = la3.getFont();
+        int fontSize3 = originalFont3.getSize() + 5; // 원하는 만큼 크기를 늘릴 수 있습니다.
+        Font largerFont3 = originalFont3.deriveFont((float) fontSize3);
+        la3.setFont(largerFont3);
+		
+		JLabel la5 = new JLabel("<html>ㅡㅡㅡㅡㅡㅡ<br> ※가로로 (한줄로 나란히) 예매해 주세요. <br>please reserve seats horizontally (in a row) </html>");
+		la5.setAlignmentX(Component.CENTER_ALIGNMENT);
+		la5.setOpaque(true);
+		la5.setBackground(Color.BLUE);
+		la5.setForeground(Color.white);
+		Border border5 = BorderFactory.createLineBorder(Color.white, 2); // 검은색 2픽셀 라인 테두리
+        la5.setBorder(border5);
+        
+        Font originalFont5 = la5.getFont();
+        int fontSize5 = originalFont5.getSize() + 15; // 원하는 만큼 크기를 늘릴 수 있습니다.
+        Font largerFont5 = originalFont5.deriveFont((float) fontSize5);
+        la5.setFont(largerFont5);
+        
 		JLabel la6 = new JLabel("Stage");
+		la6.setOpaque(true);
+		la6.setBackground(Color.lightGray);
+		la6.setForeground(Color.DARK_GRAY);
+		Border border6 = BorderFactory.createLineBorder(Color.white, 2); // 검은색 2픽셀 라인 테두리
+        la6.setBorder(border6);
+        
+        Font originalFont6 = la6.getFont();
+        int fontSize6 = originalFont6.getSize() + 10; // 원하는 만큼 크기를 늘릴 수 있습니다.
+        Font largerFont6 = originalFont6.deriveFont((float) fontSize6);
+        la6.setFont(largerFont6);
 		
+		cp2.add(la1, BorderLayout.NORTH);
 		cp2.add(la6, BorderLayout.SOUTH);
-		cp2a.add(la2);
+		la6.setHorizontalAlignment(SwingConstants.CENTER);
+
+		
+	//	cp2a.add(la2);
 		cp2a.add(la3);
-		cp2a.add(la4);
 		cp2a.add(la5);
+		
 		cp2.add(cp2a, BorderLayout.CENTER);
 		
-		for(int i=1; i<NUM_BUTTONS; i++)
-		{
-		JButton btn = new JButton("00" + Integer.toString(i));
-		cp3.add(btn);
-		}
+	      for (int i = 0; i < NUM_BUTTONS; i++) 
+	      {
+	          btn[i] = new JButton("00" + Integer.toString(i + 1));
+	          cp3.add(btn[i]);
+	       }
 		
 		cp1.add(cp2,BorderLayout.NORTH);
 		cp1.add(cp3,BorderLayout.CENTER);
 		cp.add(cp1);
 	}
 	
-	private void createSeatplacement() {
-		createToolBarRight();
-		createToolBarUpper();
-		JPanel cp1 = new JPanel();
-		JPanel cp2 = new JPanel();
-		cp1.setLayout(new BorderLayout());
-		
-		ButtonGroup group = new ButtonGroup();
-		
-		for(int i =1; i<= NUM_SEATS; i++)
-		{
-			seat[i] = new JRadioButton();
-			seat[i].setSize(1, 1);
-			group.add(seat[i]);
-			cp1.add(seat[i],BorderLayout.CENTER);
-			seat[i].setHorizontalAlignment(SwingConstants.CENTER);
-		}
-		for(int j=1; j<=10; j++)
-		{
-			JLabel Line = new JLabel(j+"열");
-			cp1.add(Line, BorderLayout.WEST);
-			Line.setHorizontalAlignment(SwingConstants.CENTER);
-		}
-	}
-	
-	
-	
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Seat();
